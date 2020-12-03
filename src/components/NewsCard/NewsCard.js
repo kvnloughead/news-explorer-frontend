20bb80bc2f5e1115c23176fc4549664d5f76cc84
Kevin Loughead <kvnloughead@gmail.com>
2020-12-03 13:19:33 -0500

    feat: NewsCard tooltips

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -9,22 +9,21 @@ function NewsCard({ card, loggedIn, index, isSaved, isMainPage }) {
             {card.keyword[0].toUpperCase() + card.keyword.slice(1)}
           </p>
           <button
-            className={`card__signin-button clickable${
-              !loggedIn && index === 2 ? ' card__signin-button_visible' : ''
-            }`}
-          >
-            Sign in to save articles
-          </button>
-          <button
             className={`card__button clickable ${
               isSaved ? ' card__button_saved' : ''
             }${!isMainPage ? ' card__button_trash' : ''}`}
           ></button>
+          <button
+            className={`card__tooltip clickable`}
+          >
+            {isMainPage ? 'Sign in to save articles' : 'Remove from saved'}
+          </button>
           <img className='card__image' src={card.image} alt={card.title} />
           <p className='card__date'>{card.date}</p>
           <h3 className='card__title'>{card.title}</h3>
           <p className='card__text'>{card.text}</p>
           <cite className='card__source'>{card.source}</cite>
+          
         </li>
       )}
     </>
