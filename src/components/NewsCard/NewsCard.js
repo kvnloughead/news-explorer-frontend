357ef7ca3e32788b919765e8b5fc50ad56363c51
Kevin Loughead <kvnloughead@gmail.com>
2020-12-04 21:52:23 -0500

    refactor: card__button into separate button tags

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -8,24 +8,33 @@ function NewsCard({ card, isMainPage, loggedIn }) {
           <p className='card__keyword'>
             {card.keyword[0].toUpperCase() + card.keyword.slice(1)}
           </p>
-          <button
-            className={`card__button clickable ${
-              card.isSaved ? ' card__button_saved' : 'card__button_not-saved'
-            }${!isMainPage ? ' card__button_trash' : ''}`}
-          ></button>
-          {(!isMainPage || !loggedIn) &&
+          {isMainPage ? (
             <button
-            className={`card__tooltip clickable`}
-          >
-            {!loggedIn ? 'Sign in to save articles' : 'Remove from saved'}
-          </button>
-          }
+              className={`card__button clickable ${
+                card.isSaved
+                  ? ' card__button_type_bookmark-saved'
+                  : 'card__button_type_bookmark'
+              }`}
+            ></button>
+          ) : (
+            <button
+              className={`card__button clickable ${
+                card.isSaved
+                  ? ' card__button_type_delete'
+                  : ''
+              }`}
+            ></button>
+          )}
+          {(!isMainPage || !loggedIn) && (
+            <button className={`card__tooltip clickable`}>
+              {!loggedIn ? 'Sign in to save articles' : 'Remove from saved'}
+            </button>
+          )}
           <img className='card__image' src={card.image} alt={card.title} />
           <p className='card__date'>{card.date}</p>
           <h3 className='card__title'>{card.title}</h3>
           <p className='card__text'>{card.text}</p>
           <cite className='card__source'>{card.source}</cite>
-          
         </li>
       )}
     </>
