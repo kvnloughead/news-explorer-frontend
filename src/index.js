f1c825ab7365a5d1edac17ec2f950df1a7fd4e42
Kevin Loughead <kvnloughead@gmail.com>
2021-01-16 11:37:23 -0500

    fix: added basename news-explorer-frontend

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/news-explorer-frontend">
       <App />
     </Router>
   </React.StrictMode>,
